76f6b1458ebc4d66ccde01de2877c51208d5167a
inwerpsel <pieter.vincent1988@gmail.com>
2021-09-24 12:23:34 +0200

    Remove commented lines and consecutive whitespace

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,7 @@
-//const merge = require('webpack-merge');
-//const common = require('./webpack.common.js');
-
-
-// npm install --save-dev webpack webpack-cli
-// npm install --save-dev babel-loader babel-core babel-preset-env
-// npm install --save-dev css-loader
-// npm install --save-dev style-loader
-// npm install --save-dev sass-loader node-sass
-// npm install --save-dev postcss-loader
-// npm install --save-dev autoprefixer
-// npm install --save-dev file-loader
-// npm install --save-dev mini-css-extract-plugin
-// npm install --save-dev optimize-css-assets-webpack-plugin
-// npm install --save-dev uglifyjs-webpack-plugin
-
 const path = require ('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
-
-
 
 module.exports = (env) => ({
     entry:  './app.js',
